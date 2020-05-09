907e81eb8b7e177b1f663d4b8b9c44d0c6543068
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-05-09 14:50:25 -0600

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAm4XhFsTNZUgHbkW7ZFnNglLA0kqlBS6k",
+  authDomain: "teclolstats.firebaseapp.com",
+  databaseURL: "https://teclolstats.firebaseio.com",
+  projectId: "teclolstats",
+  storageBucket: "teclolstats.appspot.com",
+  messagingSenderId: "20095061681",
+  appId: "1:20095061681:web:518c17a1ee148ad4931c90"
+};
 
 @NgModule({
   declarations: [
@@ -10,6 +25,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
