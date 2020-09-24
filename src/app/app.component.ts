e38119c65558bd0b818c29ae641477103d344d68
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-09-23 19:56:41 -0600

    ❌ Eliminated references and use of auth services in various controllers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 import { LoginComponent } from './widgets/login/login.component';
 import { AuthService } from './services/auth.service';
 import { SideMenuComponent } from './widgets/side-menu/side-menu.component';
+import { Controller } from './services/control/Controller.service';
 
 @Component({
   selector: 'app-root',
@@ -26,12 +27,14 @@ export class AppComponent {
     this.sideMenu.showSideMenu(true);
   }
 
-  constructor(private authService: AuthService){
+  constructor(private authService: AuthService, private controller: Controller){
   }
 
   ngOnInit() {
-    this.authService.user$.subscribe((data) => {
-      this.isLoggedIn = data ? true : false;
+    this.controller.getLoggedUser().then(res => {
+      this.isLoggedIn = true
+    }).catch(err => {
+      this.isLoggedIn = false
     })
   }
 }
--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
-import { take, map, tap } from 'rxjs/operators';
 import { Controller } from './control/Controller.service';
 
 
@@ -11,7 +9,7 @@ import { Controller } from './control/Controller.service';
 })
 export class AuthGuardService implements CanActivate{
 
-  constructor(private auth:AuthService, private controller:Controller, private router: Router) { }
+  constructor(private controller:Controller, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return new Observable(sub => {
