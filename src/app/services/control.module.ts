3775459a22c3abec5669eb294d9bdba939d76d5a
ajoscram <ajoscram@gmail.com>
2020-09-26 20:16:10 -0600

    Implemented the FirebaseAuthenticator service.

--- src/app/services/control.module.ts
+++ src/app/services/control.module.ts
@@ -3,21 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-
-var firebaseConfig = {
-    apiKey: "AIzaSyAm4XhFsTNZUgHbkW7ZFnNglLA0kqlBS6k",
-    authDomain: "teclolstats.firebaseapp.com",
-    databaseURL: "https://teclolstats.firebaseio.com",
-    projectId: "teclolstats",
-    storageBucket: "teclolstats.appspot.com",
-    messagingSenderId: "20095061681",
-    appId: "1:20095061681:web:518c17a1ee148ad4931c90"
-  };
+import { environment } from 'src/environments/environment';
 
 @NgModule({
     imports: [
         HttpClientModule,
-        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
         AngularFirestoreModule,
         AngularFireAuthModule
     ]
