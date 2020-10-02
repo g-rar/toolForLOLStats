1a46796f6ac5dd26754723497d4b52231253c1eb
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-10-02 00:56:52 -0600

    🏆➕ Made base add tournament html

--- src/app/pages/add-tournament/add-tournament.component.ts
+++ src/app/pages/add-tournament/add-tournament.component.ts
@@ -1,4 +1,7 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Controller } from 'src/app/services/control/Controller.service';
 
 @Component({
   selector: 'app-add-tournament',
@@ -9,9 +12,22 @@ export class AddTournamentComponent implements OnInit {
 
   //TODO build this
 
-  constructor() { }
+  newTournamentForm:FormGroup
+
+  constructor(private controller:Controller, private formBuilder:FormBuilder) {
+    this.newTournamentForm = formBuilder.group({
+      name: "",
+      description: "",
+      startDate: null
+    })
+  }
 
   ngOnInit(): void {
+  }
+  
+
+  addTournament(formData){
+    this.controller.addTournament(formData.name, formData.description, formData.startDate)
   }
 
 }
