132084f0abbc55154d2ce125b892bb9e68a9c655
fabioalpizar <josefabiorocks@gmail.com>
2020-10-07 18:46:25 -0600

    broken local firebase emulator testing

---
+++ src/app/services/database/FirebaseDatabase.service.spec.ts
@@ -0,0 +1,60 @@
+import MockController from './FirebaseDatabase.service';
+import FirebaseDatabase from './FirebaseDatabase.service'
+import { Tournament, Champion, Set, Match, Team, Player, } from 'src/app/models';
+import { TestBed } from '@angular/core/testing';
+import { Database } from './Database.service';
+import { AngularFirestore, SETTINGS } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
+xdescribe('FirebaseDatabase', () => {
+
+    const STUB_EXISTING_TOURNY_ID: string = "A4EwXBNykwtb4Yy4iqLK";
+
+    let database: Database;
+    let afs: AngularFirestore;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+        providers: [
+            { provide: Database, useClass: FirebaseDatabase },
+            { provide: AngularFirestore },
+            { provide: SETTINGS, useValue: {
+                host: "http://localhost:8080",
+                ssl: false
+            }}
+        ], 
+        imports: [
+            AngularFireModule.initializeApp(environment.firebaseConfig),
+            AngularFirestoreModule
+        ]
+    });
+        database = TestBed.inject(Database);
+        afs = TestBed.inject(AngularFirestore);
+    });
+
+    it('addTournament(): add a new tournament', async () => {
+        const name: string = "tourny1";
+        const description: string = "this is a new tourny";
+        const startDate: Date = new Date();
+
+        const tournament: Tournament = await database.addTournament(name, description, startDate);
+        expect(tournament).toBeTruthy();
+        expect(tournament.name).toBe(name);
+
+    });
+
+    it('endTournament(): end existing tournament', async () => {
+        const tournament: Tournament = await database.endTournament(STUB_EXISTING_TOURNY_ID);
+        expect(tournament.completed).toBeTruthy();
+    });
+
+    it('getTournament(): get exisiting tournament', async () => {
+        const tournament: Tournament = await database.getTournament(STUB_EXISTING_TOURNY_ID);
+        expect(tournament).toBeTruthy();
+        expect(tournament.id).toBe(STUB_EXISTING_TOURNY_ID);
+
+    });
+
+});
--- src/app/services/database/FirebaseDatabase.service.ts
+++ src/app/services/database/FirebaseDatabase.service.ts
@@ -1,10 +1,95 @@
+import { Tournament, Round, Champion, Set, Match, Team, Player, } from 'src/app/models';
 import { Injectable } from '@angular/core';
 import ControlModule from '../control.module';
-import { Database } from './Database.service';
+import { Database, DatabaseError } from './Database.service';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { NgSelectOption } from '@angular/forms';
 
 @Injectable({
     providedIn: ControlModule
 })
-export default class FirebaseDatabase{
+export default class FirebaseDatabase implements Database{
     
+    afsCollection: AngularFirestoreCollection<any>;
+    afsDocument: AngularFirestoreDocument<any>;
+    item: Observable<any>
+    items: Observable<any[]>;
+    //rounds: Observable<Round[]>;
+    //sets: Observable<Set[]>;
+    //matches: Observable<Match[]>;
+    //teams: Observable<Team[]>;
+    
+    constructor(private afs: AngularFirestore) {
+
+    }
+
+    async addTournament(name: string, description: string, startDate: Date): Promise<Tournament>{    
+        
+        const tempID = this.afs.createId()
+        const rounds = new Array<String>()
+        const teams = new Array<String>()
+
+        const newTournament = {
+            id: tempID,
+            name: name,
+            description: description,
+            startDate: new Date(),
+            endDate: null,
+            roundIds: rounds,
+            teamIds: teams
+        }
+
+        this.afs.doc('tournaments/' + tempID).set(newTournament).then(res => {
+            console.log('Tournament added!')
+        }).catch(err => {
+            console.log('Error adding tournament!',err)
+            throw new Error(DatabaseError.UNKNOWN_TOURNAMENT_ID);
+        });
+
+        this.item = this.afs.doc('tournaments/' + tempID).valueChanges()
+        return this.item.toPromise()
+    }
+    
+    async endTournament(id: string): Promise<Tournament>{
+        this.afs.doc('tournaments/' + id).set({
+            endDate: new Date()
+        },{ merge: true }
+        ).then(res => {
+            console.log('Tournament added!')
+        }).catch(err => {
+            console.log('Error adding tournament!',err)
+            throw new Error(DatabaseError.UNKNOWN_TOURNAMENT_ID);
+        });
+        
+        this.item = this.afs.doc('tournaments/' + id).valueChanges()
+        return this.item.toPromise()
+    }
+    
+    async getTournament(id: string): Promise<Tournament>{
+        this.item = this.afs.doc('tournaments/' + id).valueChanges()
+        return this.item.toPromise()
+    }
+    
+    async getTournaments(): Promise<Tournament[]>{
+        this.items = this.afs.collection<Tournament>('tournaments').valueChanges()
+        return this.items.toPromise()
+    }
+
+    getTournamentChampionsStats: (tournamentId: string) => Promise<any[]>; //for later
+    getTournamentChampionStats: (tournamentId: string, championId: number) => Promise<any>; //for later
+    addRound: (tournamentId: string, roundName: string) => Promise<any>;
+    deleteRound: (tournamentId: string, roundId: string) => Promise<any>;
+    getRound: (tournamentId: string, roundId: string) => Promise<any>;
+    getRounds: (tournamentId: string) => Promise<any[]>;
+    addSet: (tournamentId: string, roundId: string, firstTeamId: string, secondTeamId: string) => Promise<any>;
+    getSets: (tournamentId: string, roundId: string) => Promise<any[]>;
+    deleteSet: (tournamentId: string, roundId: string, setId: string) => Promise<any>;
+    addMatch: (tournamentId: string, roundId: string, setId: string, match: any) => Promise<any>;
+    getMatches: (tournamentId: string, roundId?: string, setId?: string) => Promise<any[]>;
+    addTeam: (tournamentId: string, name: string) => Promise<any>;
+    getTeams: (tournamentId: string) => Promise<any[]>;
+    getPlayerStats: (teamId?: string) => Promise<any[]>; //for later
+
 }
