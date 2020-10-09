fa81d145f43171be06eca9cec23638230642ba0d
Gerardo López Calderón <42049053+g-rar@users.noreply.github.com>
2020-10-09 15:15:15 -0600

    📝Added edit teams partial functionality
    Can't be removed nor renamed

--- src/app/pages/tournament/tournament.component.ts
+++ src/app/pages/tournament/tournament.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { Round, Tournament } from 'src/app/models';
+import { Round, Team, Tournament } from 'src/app/models';
 import { Controller } from 'src/app/services/control/Controller.service';
 import { ToastService } from 'src/app/services/toast/toast.service';
 
@@ -13,13 +13,14 @@ import { ToastService } from 'src/app/services/toast/toast.service';
 export class TournamentComponent implements OnInit {
 
   //TODO remove title, add description, if user logged in give report capabilities
-  //TODO add round edition
   //TODO add set edition (a set can have 0 games)
-  //TODO add teams display
 
   tournament:Tournament;
   roundList:Round[];
+  teams:Team[];
   editRoundsOverlay$:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  editTeamsOverlay$:BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  editSetsOverlay$:BehaviorSubject<number> = new BehaviorSubject<number>(-1);  
   isLoggedIn = false;
 
   constructor(private route: ActivatedRoute, private controller: Controller, private router:Router, private toast: ToastService) {
@@ -31,12 +32,27 @@ export class TournamentComponent implements OnInit {
     })
     this.route.params.subscribe(params => {
       this.controller.getTournament(params.tournamentId).then(res => {
+
         this.tournament = res;
+        console.log(res);
+        
+        
+        //rounds
         this.controller.getRounds(this.tournament.id).then(res => {
           this.roundList = res; 
         }).catch(error => {
           console.error(error)
+        });
+
+        //teams
+        this.controller.getTeams(this.tournament.id).then(res => {
+          this.teams = res
+          console.log("teams: ",res);
+          
+        }).catch(error => {
+          console.error(error);
         })
+
       }).catch(error =>{
         console.error(error);
         this.router.navigate(['/tournamentNotFound'])
@@ -44,11 +60,12 @@ export class TournamentComponent implements OnInit {
     })
   }
 
+  editTeams() {
+    this.editTeamsOverlay$.next(true)
+  }
+
   editRounds() {
     this.editRoundsOverlay$.next(true)
-    this.toast.showToast({
-      text: "Edit rounds clicked"
-    })
   }
 
   editSets(roundId: string){
--- src/app/widgets/overlays/edit-teams/edit-teams.component.ts
+++ src/app/widgets/overlays/edit-teams/edit-teams.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Tournament, Round, Team } from 'src/app/models';
+import { Controller } from 'src/app/services/control/Controller.service';
+import { ToastService } from 'src/app/services/toast/toast.service';
 
 @Component({
   selector: 'app-edit-teams',
@@ -7,9 +11,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditTeamsComponent implements OnInit {
 
-  constructor() { }
+  @Input() showOverlay$ : BehaviorSubject<boolean>;
+  @Input() tournament : Tournament;
+  @Input() teams:Team[] = []
+  teamsCopy:any[] = []
+  newTeam:string = "";
+  showOverlay = false;
+  added = 0;
 
+  constructor(private controller:Controller, private toast:ToastService) {
+  }
+  
   ngOnInit(): void {
+    this.showOverlay$.subscribe(val => {
+      this.showOverlay = val
+    })
+    this.teams.forEach(t => {
+      this.teamsCopy.push({...t} as Team)
+    })
+    console.log(this.teams);    
   }
 
+  hide() {
+    this.showOverlay$.next(false);
+  }
+
+  addTeam() {
+    if(this.newTeam === ""){
+      this.toast.showToast({
+        title:"Algo anda mal", 
+        text: "No se pueden meter Equipos sin nombre", 
+        type:"danger",
+        delay: 4000
+      })
+      return;
+    }
+    if(this.teamsCopy.find(t => t.name === this.newTeam)){
+      this.toast.showToast({
+        title:"Algo anda mal", 
+        text: "No se pueden meter dos Equipos con el mismo nombre", 
+        type:"danger",
+        delay: 4000
+      })
+      return;
+    }
+    let newTeam = {name: this.newTeam, id: "new"+this.added}
+    console.log(newTeam);
+    this.teamsCopy.push(newTeam)
+    this.added++;
+    this.newTeam = "";
+  }
+
+  remove(tm) { 
+    if(tm.players && tm.players.length != 0){
+      this.toast.showToast({
+        title:"Algo anda mal", 
+        text: "No se pueden remover Equipos con Jugadores registrados", 
+        type:"danger",
+        delay: 4000
+      })
+      return;
+    }
+
+    this.teamsCopy.splice(this.teamsCopy.findIndex(t => t.name === tm.name),1)
+  }
+
+  async saveTeams() {
+    this.toast.showToast({text:"Guardando modificaciones. Espere un momento.", title:"💾 Guardando", delay: 5000})
+    //TODO improve this
+    for(let i = 0 ; i < this.teamsCopy.length ; i++){
+      //if round existed previously with a different name and has no sets
+      let originalTeam = this.teams.find(t => 
+        t.id === this.teamsCopy[i].id 
+        // && t.name !== this.teamsCopy[i].name
+      );
+      // if(originalTeam && originalTeam.players.length !== 0){
+      //   //TODO request for editing objects so this is more possible 
+      //   //TODO request for deleting teams
+      //   await this.controller.addTeam(this.tournament.id, originalRound.id)
+      //   await this.controller.addRound(this.tournament.id, this.roundsCopy[i].name)
+      
+      // } else 
+      if (!originalTeam && !this.teamsCopy[i].sets){
+        //if its a new team, it won't have players, needs to be added
+        await this.controller.addTeam(this.tournament.id, this.teamsCopy[i].name)
+      }
+    }
+    this.toast.showToast({
+      text:"Cambios guardados. Es posible que tengas que recargar la página.",
+      title:"💾✅ Completado",
+      type:"notification"
+    })
+    this.showOverlay$.next(false)
+    this.teamsCopy = [];
+    this.teams.forEach(tm => {
+      this.teamsCopy.push({...tm} as Team)
+    })
+  }
 }
